1130a3221dd117262b70047612f10bbef676fc8a
Gavin Mason <gmasonwrk@gmail.com>
2023-04-27 10:10:13 -0400

    Updated calendar

--- components/Calendar.vue
+++ components/Calendar.vue
@@ -7,7 +7,6 @@
       </h1>
       <div class="flex items-center">
         <div class="flex items-center">
-          <!-- <button type="button" class="ml-6 rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500">Do Something</button> -->
           <button type="button" @click="subMonth()" class="h-4 w-4">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
@@ -34,8 +33,8 @@
       </div>
       <div class="flex bg-gray-200 text-xs leading-6 text-gray-700 lg:flex-auto">
         <div class="grid w-full h-[75vh] grid-cols-7 grid-rows-6 gap-px">
-          <button @click="$emit('add'); store.updateDay(day) " v-for="day in dates" :key="day" type="button" :class="[day.isCurrentMonth ? 'bg-white' : 'bg-gray-50', (day.isSelected || currentDate) && 'font-semibold', day.isSelected && 'text-white', !day.isSelected && currentDate && 'text-blue-600', !day.isSelected && day.isCurrentMonth && !currentDate && 'text-gray-900', !day.isSelected && !day.isCurrentMonth && !currentDate && 'text-gray-500', 'flex h-auto flex-col px-3 py-2 hover:bg-gray-100']"> 
-              <time :datetime="day" :class="[day.isSelected && 'flex h-6 w-6 items-center justify-center rounded-full', day.isSelected && currentDate && 'bg-blue-600', day.isSelected && !currentDate && 'bg-gray-900', 'ml-auto']">{{ day.split('-').slice(-2).reverse().pop().replace(/^0/, '') }}</time> 
+          <button @click="$emit('add'); store.updateDay(day) " v-for="day in dates" :key="day.value" type="button" :class="[day.split('-').slice(-4).reverse().pop().replace(/^0/, '') === currentMonth ? 'bg-white' : 'bg-gray-100', 'flex h-auto flex-col px-3 py-2 hover:bg-gray-100']"> 
+              <time :datetime="day" :class="[day == currentDate && 'flex h-6 w-6 items-center justify-center rounded-full bg-orange-400', 'ml-auto']">{{ day.split('-').slice(-2).reverse().pop().replace(/^0/, '') }}</time>  <!--  -->
 
             <!-- <span class="sr-only">{{ day.events.length }} events</span>
             <span v-if="day.events.length > 0" class="-mx-0.5 mt-auto flex flex-wrap-reverse">
@@ -61,7 +60,7 @@ let date = new Date()
 let currentDay = date.toLocaleString("default", {day: "2-digit"})//.padStart(2, '0');
 let currentMonth = ref(date.toLocaleString("default", {month: "numeric"})) //.padStart(2,"0");
 let currentYear = ref(date.toLocaleString("default", {year: "numeric"})) 
-let currentDate = currentMonth + '-' + currentDay + '-' + currentYear
+let currentDate = currentMonth.value + '-' + currentDay + '-' + currentYear.value
 
 
 // const selectedDay = days.find((day) => day.isSelected)
@@ -70,37 +69,48 @@ let currentDate = currentMonth + '-' + currentDay + '-' + currentYear
 
 // console.log(getDays())
 let dates = ref([])
-let datesBefore = []
-let datesAfter = []
+// let datesCurrent = []
+
 
 const getDays = () => {
-  dates.value.splice(0, dates.value.length)
-  // let firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay()
-  let lastDateOfMonth = new Date(currentYear.value, parseInt(currentMonth.value), 0).getDate()
-  // let lastDayOfMonth = new Date(currentYear, currentMonth, lastDateOfMonth).getDay()
-  // let lastDateOfLastMonth = new Date(currentYear, currentMonth, 0).getDate()
+  // Clear Array
+  dates.value.splice(0)
 
+  // Init Arrays
+  let datesCurrent = []
+  let datesBefore = []
+  let datesAfter = []
 
-  // for(let i = firstDayOfMonth; i > 0; i--)
-  // {
-  //   datesBefore.push(lastDateOfLastMonth - i + 1)
-  // }
+  // Get date baselines
+  let firstDayOfMonth = new Date(currentYear.value, currentMonth.value, 1).getDay()
+  let lastDateOfMonth = new Date(currentYear.value, parseInt(currentMonth.value) + 1, 0).getDate()
+  let lastDayOfMonth = new Date(currentYear.value, currentMonth.value, lastDateOfMonth).getDay()
+  let lastDateOfLastMonth = new Date(currentYear.value, currentMonth.value, 0).getDate()
+
+  // Get our dates and days before/after current month
+  for(let i = firstDayOfMonth; i > 0; i--)
+  {
+    datesBefore.push((parseInt(currentMonth.value) - 1) + '-' + (parseInt(lastDateOfLastMonth) - i + 1) + '-' + currentYear.value)
+  }
   // console.log(datesBefore)
 
-  // console.log('lastDateofmonth', lastDateOfMonth)
-  for(let i=0; i <=lastDateOfMonth-1; i++)
+  for(let i=1; i <= lastDateOfMonth - 1; i++)
   {
-    dates.value[i] = ( currentMonth.value + '-' + (i+1) + '-' + currentYear.value)
+    datesCurrent.push( currentMonth.value + '-' + (i) + '-' + currentYear.value)
   } 
+
+  for(let i = lastDayOfMonth; i < 6; i++)
+  {
+    datesAfter.push( (parseInt(currentMonth.value) + 1) + '-' + (i - lastDayOfMonth + 1) + '-' + currentYear.value)
+  }
+  console.log(currentDate.split('-').slice(-4).reverse().pop().replace(/^0/, ''))
+  // console.log(lastDateOfMonth)
+  // console.log(lastDayOfMonth)
+
+  // Combine results
+  datesCurrent = datesCurrent.concat(datesAfter)
+  dates.value = datesBefore.concat(datesCurrent)
   console.log(dates.value)
-  console.log(currentMonth.value)
-  console.log(currentYear.value)
-  // for(let i= lastDayOfMonth; i < 6; i++)
-  // {
-  //   datesAfter.push(i - lastDateOfMonth + 1)
-  // }
-  // // console.log(lastDay)
-  // console.log(datesAfter)
 }
 getDays()
 
