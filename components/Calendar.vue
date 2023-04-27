a61a3ef1660c17eaaaeda040ee066a664b3fae8e
Gavin Mason <gmasonwrk@gmail.com>
2023-04-27 06:10:41 -0400

    Updated calendar with real dates

--- components/Calendar.vue
+++ components/Calendar.vue
@@ -3,11 +3,22 @@
   <div class="px-10 lg:flex lg:h-full lg:flex-col">
     <header class="flex items-center justify-between border-b border-gray-200 px-6 py-4 lg:flex-none">
       <h1 class="text-base font-semibold leading-6 text-gray-900">
-        <time datetime="2022-01">January 2022</time>
+        <time datetime="2022-01">{{ months[currentMonth-1] }} {{ currentYear }}</time>
       </h1>
       <div class="flex items-center">
-        <div class="md:ml-4 md:flex md:items-center">
-          <button type="button" class="ml-6 rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500">Do Something</button>
+        <div class="flex items-center">
+          <!-- <button type="button" class="ml-6 rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500">Do Something</button> -->
+          <button type="button" @click="subMonth()" class="h-4 w-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+            </svg>
+          </button>
+          <div class="px-3" />
+          <button type="button" @click="addMonth()" class="h-4 w-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+            </svg>
+          </button>
         </div>
       </div>
     </header>
@@ -22,13 +33,15 @@
         <div class="bg-white py-2">S<span class="sr-only sm:not-sr-only">at</span></div>
       </div>
       <div class="flex bg-gray-200 text-xs leading-6 text-gray-700 lg:flex-auto">
-        <div class="isolate grid w-full h-[75vh] grid-cols-7 grid-rows-6 gap-px">
-          <button @click="$emit('add'); store.updateDay(day) " v-for="day in days" :key="day.date" type="button" :class="[day.isCurrentMonth ? 'bg-white' : 'bg-gray-50', (day.isSelected || day.isToday) && 'font-semibold', day.isSelected && 'text-white', !day.isSelected && day.isToday && 'text-blue-600', !day.isSelected && day.isCurrentMonth && !day.isToday && 'text-gray-900', !day.isSelected && !day.isCurrentMonth && !day.isToday && 'text-gray-500', 'flex h-auto flex-col px-3 py-2 hover:bg-gray-100 focus:z-10']">
-              <time :datetime="day.date" :class="[day.isSelected && 'flex h-6 w-6 items-center justify-center rounded-full', day.isSelected && day.isToday && 'bg-blue-600', day.isSelected && !day.isToday && 'bg-gray-900', 'ml-auto']">{{ day.date.split('-').pop().replace(/^0/, '') }}</time>
+        <div class="grid w-full h-[75vh] grid-cols-7 grid-rows-6 gap-px">
+          <button @click="$emit('add'); store.updateDay(day) " v-for="day in dates" :key="day" type="button" :class="[day.isCurrentMonth ? 'bg-white' : 'bg-gray-50', (day.isSelected || currentDate) && 'font-semibold', day.isSelected && 'text-white', !day.isSelected && currentDate && 'text-blue-600', !day.isSelected && day.isCurrentMonth && !currentDate && 'text-gray-900', !day.isSelected && !day.isCurrentMonth && !currentDate && 'text-gray-500', 'flex h-auto flex-col px-3 py-2 hover:bg-gray-100']"> 
+              <time :datetime="day" :class="[day.isSelected && 'flex h-6 w-6 items-center justify-center rounded-full', day.isSelected && currentDate && 'bg-blue-600', day.isSelected && !currentDate && 'bg-gray-900', 'ml-auto']">{{ day.split('-').slice(-2).reverse().pop().replace(/^0/, '') }}</time> 
+
             <!-- <span class="sr-only">{{ day.events.length }} events</span>
             <span v-if="day.events.length > 0" class="-mx-0.5 mt-auto flex flex-wrap-reverse">
               <span v-for="event in day.events" :key="event.id" class="mx-0.5 mb-1 h-1.5 w-1.5 rounded-full bg-gray-400" />
             </span> -->
+
           </button>
         </div>
       </div>
@@ -41,65 +54,72 @@ import { useCounterStore } from '@/stores/store'
 
 const store = useCounterStore()
 
-const days = [
-  { date: '2021-12-27'},
-  { date: '2021-12-28'},
-  { date: '2021-12-29'},
-  { date: '2021-12-30'},
-  { date: '2021-12-31'},
-  { date: '2022-01-01', isCurrentMonth: true},
-  { date: '2022-01-02', isCurrentMonth: true},
+const months = ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"]
+
+let date = new Date()
+let currentDay = date.toLocaleString("default", {day: "2-digit"})//.padStart(2, '0');
+let currentMonth = ref(date.toLocaleString("default", {month: "numeric"})) //.padStart(2,"0");
+let currentYear = ref(date.toLocaleString("default", {year: "numeric"})) 
+let currentDate = currentMonth + '-' + currentDay + '-' + currentYear
+
+
+// const selectedDay = days.find((day) => day.isSelected)
+
+// console.log(parseInt(currentMonth.value) + 1)
+
+// console.log(getDays())
+let dates = ref([])
+let datesBefore = []
+let datesAfter = []
+
+const getDays = () => {
+  dates.value.splice(0, dates.value.length)
+  // let firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay()
+  let lastDateOfMonth = new Date(currentYear.value, parseInt(currentMonth.value), 0).getDate()
+  // let lastDayOfMonth = new Date(currentYear, currentMonth, lastDateOfMonth).getDay()
+  // let lastDateOfLastMonth = new Date(currentYear, currentMonth, 0).getDate()
+
+
+  // for(let i = firstDayOfMonth; i > 0; i--)
+  // {
+  //   datesBefore.push(lastDateOfLastMonth - i + 1)
+  // }
+  // console.log(datesBefore)
+
+  // console.log('lastDateofmonth', lastDateOfMonth)
+  for(let i=0; i <=lastDateOfMonth-1; i++)
   {
-    date: '2022-01-03',
-    isCurrentMonth: true,
-  },
-  { date: '2022-01-04', isCurrentMonth: true},
-  { date: '2022-01-05', isCurrentMonth: true},
-  { date: '2022-01-06', isCurrentMonth: true},
-  {
-    date: '2022-01-07',
-    isCurrentMonth: true,
-  },
-  { date: '2022-01-08', isCurrentMonth: true},
-  { date: '2022-01-09', isCurrentMonth: true},
-  { date: '2022-01-10', isCurrentMonth: true},
-  { date: '2022-01-11', isCurrentMonth: true},
-  {
-    date: '2022-01-12',
-    isCurrentMonth: true,
-    isToday: true,
-  },
-  { date: '2022-01-13', isCurrentMonth: true},
-  { date: '2022-01-14', isCurrentMonth: true},
-  { date: '2022-01-15', isCurrentMonth: true},
-  { date: '2022-01-16', isCurrentMonth: true},
-  { date: '2022-01-17', isCurrentMonth: true},
-  { date: '2022-01-18', isCurrentMonth: true},
-  { date: '2022-01-19', isCurrentMonth: true},
-  { date: '2022-01-20', isCurrentMonth: true},
-  { date: '2022-01-21', isCurrentMonth: true},
-  {
-    date: '2022-01-22',
-    isCurrentMonth: true,
-    isSelected: true,
-  },
-  { date: '2022-01-23', isCurrentMonth: true},
-  { date: '2022-01-24', isCurrentMonth: true},
-  { date: '2022-01-25', isCurrentMonth: true},
-  { date: '2022-01-26', isCurrentMonth: true},
-  { date: '2022-01-27', isCurrentMonth: true},
-  { date: '2022-01-28', isCurrentMonth: true},
-  { date: '2022-01-29', isCurrentMonth: true},
-  { date: '2022-01-30', isCurrentMonth: true},
-  { date: '2022-01-31', isCurrentMonth: true},
-  { date: '2022-02-01'},
-  { date: '2022-02-02'},
-  { date: '2022-02-03'},
-  {
-    date: '2022-02-04',
-  },
-  { date: '2022-02-05'},
-  { date: '2022-02-06'},
-]
-const selectedDay = days.find((day) => day.isSelected)
+    dates.value[i] = ( currentMonth.value + '-' + (i+1) + '-' + currentYear.value)
+  } 
+  console.log(dates.value)
+  console.log(currentMonth.value)
+  console.log(currentYear.value)
+  // for(let i= lastDayOfMonth; i < 6; i++)
+  // {
+  //   datesAfter.push(i - lastDateOfMonth + 1)
+  // }
+  // // console.log(lastDay)
+  // console.log(datesAfter)
+}
+getDays()
+
+function addMonth(){
+  currentMonth.value = (parseInt(currentMonth.value) + 1).toString()
+  if(currentMonth.value > 12){
+    currentMonth.value = 1
+    currentYear.value = (parseInt(currentYear.value) + 1).toString()
+  }
+  getDays()
+}
+
+function subMonth(){
+  currentMonth.value = (parseInt(currentMonth.value) - 1).toString()
+  if(currentMonth.value < 1){
+    currentMonth.value = 12
+    currentYear.value = (parseInt(currentYear.value) - 1).toString()
+  }
+  getDays()
+}
+
 </script>
